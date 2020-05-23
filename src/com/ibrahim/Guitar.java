86c906b5155510bc8bd8851e3eae54314f465af4
Ibrahim Jimoh <ib.omogbolahan@yahoo.com>
2020-05-23 21:38:20 +0100

    first commit

---
+++ src/com/ibrahim/Guitar.java
@@ -0,0 +1,29 @@
+package com.ibrahim;
+
+public class Guitar extends Instrument {
+
+    private int numberOfStrings;
+    private double stringSize;
+
+    public Guitar(String manufacturer, int numberOfStrings, double stringSize) {
+        super(manufacturer);
+        this.numberOfStrings = numberOfStrings;
+        this.stringSize = stringSize;
+    }
+
+    public int getNumberOfStrings() {
+        return numberOfStrings;
+    }
+
+    public void setNumberOfStrings(int numberOfStrings) {
+        this.numberOfStrings = numberOfStrings;
+    }
+
+    public double getStringSize() {
+        return stringSize;
+    }
+
+    public void setStringSize(double stringSize) {
+        this.stringSize = stringSize;
+    }
+}
