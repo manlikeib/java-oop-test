8b9403cbd3061eb50e1b53ca213422262a980d71
Ibrahim Jimoh <ib.omogbolahan@yahoo.com>
2020-05-24 10:38:40 +0100

    Testing the inheritance between base and sub classes

---
+++ src/com/ibrahim/Main.java
@@ -0,0 +1,10 @@
+package com.ibrahim;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Testing the inheritance between base and sub classes
+        Banjo banjo = new Banjo("HP",26,0.8);
+        System.out.println(banjo.getClass().getSimpleName());
+    }
+}
