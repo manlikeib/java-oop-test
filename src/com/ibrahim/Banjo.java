188eaff4618eaf8fdf59bffdcc5161e350ac8262
Ibrahim Jimoh <ib.omogbolahan@yahoo.com>
2020-05-24 10:57:20 +0100

    Added toString() method.

--- src/com/ibrahim/Banjo.java
+++ src/com/ibrahim/Banjo.java
@@ -5,7 +5,7 @@ public class Banjo extends Guitar{
     public Banjo(String manufacturer, int numberOfStrings, double stringSize) {
         super(manufacturer, numberOfStrings, stringSize);
     }
-    
+
     @Override
     public String toString() {
         return "Banjo{" +
